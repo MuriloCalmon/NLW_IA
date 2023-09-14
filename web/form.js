7af53bd3cf37eee11db9f4a4a5d48ebaff435293
MuriloCalmon <murilocalmon80@gmail.com>
2023-09-14 09:18:03 -0300

    aula 2- Back End

---
+++ web/form.js
@@ -0,0 +1,33 @@
+import { server } from "./server"
+
+const form = document.querySelector("#form")
+const input = document.querySelector("#url")
+const content = document.querySelector("#content")
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault()
+
+  content.classList.add("placeholder")
+
+  const videoUrl = input.value
+
+  if (!videoUrl.includes("shorts")) {
+    return (content.textContent = "Esse vídeo não é um short.")
+  }
+
+  const [_, params] = videoUrl.split("/shorts/")
+  const [videoId] = params.split("?si")
+
+  content.textContent = "Obtendo o texto do vídeo..."
+
+  const transcription = await server.get("/summary/" + videoId)
+
+  content.textContent = "Realizando o resumo"
+
+  const summary = await server.post("/summary", () => {
+    text: transcription.data.result
+  })
+
+  content.textContent = summary.data.result
+  content.classList.remove("placeholder")
+})
